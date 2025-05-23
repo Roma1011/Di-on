ab92e0b8664eaa42321a2910f83f066be4a8696f
Roma1011 <romski48@gmail.com>
2025-05-23 23:44:25 +0400

    Enhance AggregateLifeTime method with improved logic and detailed XML documentation

--- Aggregator/LifetimeAggregator.cs
+++ Aggregator/LifetimeAggregator.cs
@@ -3,84 +3,92 @@ using DiÆon.Attributes;
 using DiÆon.Attributes.Base;
 using DiÆon.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DiÆon.Aggregator;
 
 public static class Aggregator
 {
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Scans the provided assembly for non-interface types annotated with 
+    /// lifetime attributes (<see cref="Singleton"/>, <see cref="Scoped"/>, or <see cref="Transient"/>),
+    /// and registers them in the <see cref="IServiceCollection"/> with the corresponding service lifetime.
+    /// 
+    /// If a type is decorated with more than one lifetime attribute, a <see cref="MultipleLifetimeException"/> is thrown.
+    /// The method also attempts to find a corresponding interface annotated with the same attribute to use for registration,
+    /// otherwise it registers the implementation type as itself.
+    /// </summary>
+    /// <param name="collection">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+    /// <param name="assembly">The assembly to scan for types with lifetime attributes.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/> with all discovered and registered services.</returns>
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     public static IServiceCollection AggregateLifeTime(this IServiceCollection collection,Assembly assembly)
     {
-        var definedTypes = assembly.DefinedTypes.
+        IEnumerable<TypeInfo> typesWithLifetimeAttributes = assembly.DefinedTypes.
             Where(definedTypes => !definedTypes.IsInterface && definedTypes.CustomAttributes.
-                    Any(x
-                        =>
-                            x.AttributeType==typeof(Singleton) || 
-                            x.AttributeType==typeof(Scoped)    || 
-                            x.AttributeType==typeof(Transient)
-                    )
+                Any(attr
+                    =>
+                    attr.AttributeType==typeof(Singleton) || 
+                    attr.AttributeType==typeof(Scoped)    || 
+                    attr.AttributeType==typeof(Transient)
+                )
             );
         
-        foreach (var defined in definedTypes)
+        foreach (var typeInfo in typesWithLifetimeAttributes)
         {
-            if(defined.CustomAttributes.Count(x => x.AttributeType.BaseType == typeof(BaseLifetimeAttribute)) > 1)
+            if(typeInfo.CustomAttributes.Count(x => x.AttributeType.BaseType == typeof(BaseLifetimeAttribute)) > 1)
                 throw new MultipleLifetimeException();
             
-            if (defined.CustomAttributes.Any(custom => custom.AttributeType == typeof(Singleton)))
-            {
-                var declaredInterface=defined.ImplementedInterfaces.
-                    FirstOrDefault(type=>type.CustomAttributes.
-                        Any(customAttributeData => customAttributeData.AttributeType==typeof(Singleton)));
-                
-                if (declaredInterface is not null)
-                {
-                    if (declaredInterface.IsGenericType)
-                        declaredInterface = declaredInterface.GetGenericTypeDefinition();
-                    
-                    collection.TryAdd(new ServiceDescriptor(declaredInterface,defined,ServiceLifetime.Singleton));
-                } else {
-                    collection.TryAdd(new ServiceDescriptor(defined,defined,ServiceLifetime.Singleton));
-                }
-                    
-                continue;
-            }
-            
-            if (defined.CustomAttributes.Any(custom => custom.AttributeType == typeof(Scoped)))
-            {
-                var declaredInterface=defined.ImplementedInterfaces.
-                    FirstOrDefault(type=>type.CustomAttributes.
-                        Any(customAttributeData => customAttributeData.AttributeType==typeof(Scoped)));
+            var lifetimeAttributeType = typeInfo.CustomAttributes
+                .First(attr =>
+                    attr.AttributeType == typeof(Scoped)    ||
+                    attr.AttributeType == typeof(Transient) ||
+                    attr.AttributeType == typeof(Singleton)).AttributeType;
 
-                if (declaredInterface is not null)
-                {
-                    if (declaredInterface.IsGenericType)
-                        declaredInterface = declaredInterface.GetGenericTypeDefinition();
-                    
-                    collection.TryAdd(new ServiceDescriptor(declaredInterface,defined,ServiceLifetime.Scoped));
-                } else {
-                    collection.TryAdd(new ServiceDescriptor(defined,defined,ServiceLifetime.Scoped));
-                }
-                
-                continue;
-            }
-            
-            if (defined.CustomAttributes.Any(custom => custom.AttributeType == typeof(Transient)))
-            {
-                var declaredInterface=defined.ImplementedInterfaces.
-                    FirstOrDefault(type=>type.CustomAttributes.
-                        Any(customAttributeData => customAttributeData.AttributeType==typeof(Transient)));
-
-                if (declaredInterface is not null)
-                {
-                    if (declaredInterface.IsGenericType)
-                        declaredInterface = declaredInterface.GetGenericTypeDefinition();
-                    
-                    collection.TryAdd(new ServiceDescriptor(declaredInterface, defined, ServiceLifetime.Transient));
-                } else {
-                    collection.TryAdd(new ServiceDescriptor(defined,defined,ServiceLifetime.Transient));
-                }
-            }
+            var descriptor = LifeSeeker(typeInfo, lifetimeAttributeType);
+            collection.Add(descriptor);
         }
         return collection;
     }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+    /// <summary>
+    /// Creates a <see cref="ServiceDescriptor"/> based on the specified implementation type and its associated lifetime attribute.
+    /// 
+    /// If the implementation type implements an interface that is also annotated with the same lifetime attribute,
+    /// that interface will be used as the service type; otherwise, the implementation type is used as its own service type.
+    /// </summary>
+    /// <param name="implementationType">The concrete implementation type to be registered.</param>
+    /// <param name="lifetimeAttributeType">The type of the lifetime attribute (e.g., <see cref="Scoped"/>, <see cref="Transient"/>, or <see cref="Singleton"/>).</param>
+    /// <returns>A <see cref="ServiceDescriptor"/> representing the service registration.</returns>
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static ServiceDescriptor LifeSeeker(TypeInfo implementationType,Type lifetimeAttributeType)
+    {
+        Type? serviceInterface=implementationType.ImplementedInterfaces.
+            FirstOrDefault(type=>type.CustomAttributes.
+                Any(customAttributeData => customAttributeData.AttributeType==lifetimeAttributeType));
+                
+        if (serviceInterface is not null)
+        {
+            if (serviceInterface.IsGenericType)
+                serviceInterface = serviceInterface.GetGenericTypeDefinition();
+                    
+            return new ServiceDescriptor(serviceInterface,implementationType,GetLifetimeFromAttribute(lifetimeAttributeType));
+        } else {
+            return new ServiceDescriptor(implementationType,implementationType,GetLifetimeFromAttribute(lifetimeAttributeType));
+        }
+    }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Maps a lifetime attribute type to a corresponding <see cref="ServiceLifetime"/> enum value.
+    /// </summary>
+    /// <param name="attributeType">The type of the attribute (must be <see cref="Scoped"/>, <see cref="Transient"/>, or <see cref="Singleton"/>).</param>
+    /// <returns>The corresponding <see cref="ServiceLifetime"/>.</returns>
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    private static ServiceLifetime GetLifetimeFromAttribute(Type attributeType)
+    {
+        return attributeType == typeof(Scoped)    ? ServiceLifetime.Scoped    :
+               attributeType == typeof(Transient) ? ServiceLifetime.Transient :
+            ServiceLifetime.Singleton;
+    }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 }
